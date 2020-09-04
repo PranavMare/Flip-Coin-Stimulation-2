0a27cda722d39ff8b6b26f5807a96d34a1c2cc69
Pranav <pranavmare99@gmail.com>
2020-09-05 00:34:40 +0530

    Singlet in Dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,38 @@
-#!/bin/bash -x
+#! /bin/bash
 
-result=$((RANDOM%2))
-
-if [ $result -eq 1 ]
+randomCheck=$((RANDOM%2))
+if [[ $randomCheck -eq 1 ]]
 then
-	echo "Heads"
+    echo "Heads"
 else
-	echo "Tails"
+    echo "Tail"
 fi
+
+declare -A Coin
+H=0
+T=0
+
+for (( i=0 ; i<10 ; i++ ))
+do
+    rand_singlet_check=$((RANDOM%2))
+    case $rand_singlet_check in 
+        1 )
+            H=$((H+1))
+            Coin[H]=$H;;
+        0 )
+            T=$((T+1))
+            Coin[T]=$T;;
+    esac
+done
+
+for index in ${!Coin[@]}
+do
+    echo "$index - ${Coin[$index]}"
+    if [[ $index -eq H ]]
+    then
+        echo "Singlet Head Percentage H : `echo "scale=1; ${Coin[$index]}*100/10" | bc ` %" 
+        
+    else
+        echo "Singlet Head Percentage H : `echo "scale=1; ${Coin[$index]}*100/10" | bc ` %"
+    fi
+done
